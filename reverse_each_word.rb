7ad844dabe19b566e377b423cf6b3a03608192c7
Alex Bonwit <alexbonwit@mac.com>
2019-11-15 05:27:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,11 @@ require 'pry'
 
 def reverse_each_word(string)
 # binding.pry
+  reverse_array = []
   word_array = string.split(' ')
   word_array.each do |word|
-    word.reverse
+    reverse_array << word.reverse
   end
-  word_array.join(' ')
-  word_array
+  reverse_array.join(' ')
+  reverse_array
 end
